8b837e32e11faba91ea922af0d89a4b26e3d7ef2
mremond <mremond@d5c1f840-3623-0410-9cd6-eede35f65a94>
2007-01-26 10:53:11 +0000

    * Removed hardcoded path.

--- epeios/trunk/build.sh
+++ epeios/trunk/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 erlc lib/builder.erl 
-/home/mremond/tmp/build/bin/erl -noshell -s make all -s builder go -s init stop
+erl -noshell -s make all -s builder go -s init stop
 cp modules/*.beam ebin
 cp lib/*.beam ebin
 
