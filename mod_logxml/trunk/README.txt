040f159f082ecca7e34fe065c88307d304becf42
badlop <badlop@d5c1f840-3623-0410-9cd6-eede35f65a94>
2007-02-08 15:57:21 +0000

    mod_logxml: First commit

---
+++ mod_logxml/trunk/README.txt
@@ -0,0 +1,94 @@
+
+	mod_logxml - Log XMPP packets to XML file
+
+	Homepage: http://ejabberd.jabber.ru/mod_logxml
+	Author: Badlop
+	Version: 0.2.1 - 2006-08-08
+	Module for ejabberd 0.7.5 or newer
+
+
+	DESCRIPTION
+	-----------
+
+This module sniffs all the XMPP traffic send and received by ejabberd,
+both internally and externally transmitted. It logs the XMPP packets
+to a XML formatted file. It's posible to filter transmitted packets 
+by orientation, stanza and direction. It's possible to configure the 
+file rotation rules and intervals.
+
+This module reuses code from mod_log_forensic, mod_stats2file, mod_muc_log
+
+
+	INSTALL
+	-------
+
+ 1 Copy this file to ejabberd/src/mod_logxml.erl
+ 2 Recompile ejabberd
+ 3 Add to ejabberd.cfg, 'modules' section the basic configuration:
+    {mod_logxml,     []},
+
+
+	CONFIGURABLE PARAMETERS
+	-----------------------
+
+stanza: 
+    Log packets only when stanza matches
+    Default vaue: [iq, message, presence, other]
+direction: 
+    Log packets only when direction matches
+    Default vaue: [internal, vhosts, external]
+orientation: 
+    Log packets only when orientation matches
+    Default vaue: [send, revc]
+logdir: 
+    Base filename, including absolute path
+    Default vaue: "/tmp/jabberlogs/"
+rotate_days: 
+    Rotate logs every X days
+    Put 'no' to disable this limit.
+    Default vaue: 1
+rotate_megs: 
+    Rotate when the logfile size is higher than this, in megabytes.
+    Put 'no' to disable this limit.
+    Default vaue: 10
+rotate_kpackets: 
+    Rotate every *1000 XMPP packets logged
+    Put 'no' to disable this limit.
+    Default vaue: 10
+check_rotate_kpackets: 
+    Check rotation every *1000 packets
+    Default vaue: 1
+
+
+	EXAMPLE CONFIGURATION
+	---------------------
+
+  {mod_logxml, [
+     {stanza, [message, other]},
+     {direction, [external]},
+     {orientation, [send, recv]},
+     {logdir, "/var/jabber/logs/"},
+     {rotate_days, 1}, 
+     {rotate_megs, 100}, 
+     {rotate_kpackets, no},
+     {check_rotate_kpackets, 1}
+  ]},
+
+
+	CHANGELOG
+	---------
+
+0.2.1 - 2006-08-08
+  * Fixed small bug on start/2 
+
+0.2 - 2006-03-08
+  * Changed some configuration options: 
+      rotate_days, rotate_mages and rotate_kpackets can now be set independently
+  * New format of XML logs: now XMPP packets are enclosed in <packet>, with attributes:
+      or: orientation of the packet, either 'send' or 'recv'
+      ljid: local JID of the sender or receiver, depending on the orientation
+      ts: timestamp when the packet was logged
+
+0.1 - 2005-11-11
+  * Initial version
+
---
+++ mod_logxml/trunk/src/mod_logxml.erl
@@ -0,0 +1,228 @@
+%%%----------------------------------------------------------------------
+%%% File    : mod_logxml.erl
+%%% Author  : Badlop
+%%% Purpose : Log XMPP packets to XML file
+%%% Created : 
+%%% Id      : 0.2.1
+%%%----------------------------------------------------------------------
+
+-module(mod_logxml).
+-author('').
+-vsn('').
+
+-behaviour(gen_mod).
+
+-export([start/2, init/5, stop/1,
+	send_packet/3, receive_packet/4]).
+
+-include("ejabberd.hrl").
+-include("jlib.hrl").
+
+-define(PROCNAME, ejabberd_mod_logxml).
+
+%% -------------------
+%% Module control
+%% -------------------
+
+start(Host, Opts) ->
+	Logdir = gen_mod:get_opt(logdir, Opts, "/tmp/jabberlogs/"),
+
+	Rd = gen_mod:get_opt(rotate_days, Opts, 1),
+	Rf = case gen_mod:get_opt(rotate_megs, Opts, 10) of
+		no -> no;
+		Rf1 -> Rf1*1024*1024
+	end,
+	Rp = case gen_mod:get_opt(rotate_kpackets, Opts, 10) of
+		no -> no;
+		Rp1 -> Rp1*1000
+	end,
+	RotateO = {Rd, Rf, Rp},
+	CheckRKP = gen_mod:get_opt(check_rotate_kpackets, Opts, 1),
+
+	Orientation = gen_mod:get_opt(orientation, Opts, [send, recv]),
+	Stanza = gen_mod:get_opt(stanza, Opts, [iq, message, presence, other]),
+	Direction = gen_mod:get_opt(direction, Opts, [internal, vhosts, external]),
+	FilterO = {
+		{orientation, Orientation},
+		{stanza, Stanza},
+		{direction, Direction}},
+
+	ejabberd_hooks:add(user_send_packet, Host, ?MODULE, send_packet, 90),
+	ejabberd_hooks:add(user_receive_packet, Host, ?MODULE, receive_packet, 90),
+    register(gen_mod:get_module_proc(Host, ?PROCNAME),
+		spawn(?MODULE, init, [Host, Logdir, RotateO, CheckRKP, FilterO])).
+
+stop(Host) ->
+    ejabberd_hooks:delete(user_send_packet, Host, ?MODULE, send_packet, 60),
+    ejabberd_hooks:delete(user_receive_packet, Host, ?MODULE, receive_packet, 60),
+    Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+    Proc ! stop,
+    {wait, Proc}.
+
+init(Host, Logdir, RotateO, CheckRKP, FilterO) ->
+	{IoDevice, Filename, Gregorian_day} = open_file(Logdir, Host),
+	loop(Host, IoDevice, Filename, Logdir, CheckRKP, RotateO, 0, Gregorian_day, FilterO).
+
+%% -------------------
+%% Main
+%% -------------------
+
+manage_rotate(Host, IoDevice, Filename, Logdir, RotateO, PacketC, Gregorian_day_log) ->
+	{RO_days, RO_size, RO_packets} = RotateO,
+
+	Rotate1 = case RO_packets of
+		no -> false;
+		PacketC -> true;
+		_ -> false
+	end,
+	
+	Filesize = filelib:file_size(Filename),
+	Rotate2 = if 
+		RO_size == no -> false;
+		Filesize >= RO_size -> true;
+		true -> false
+	end,
+	
+	Gregorian_day_today = get_gregorian_day(),
+	Rotate3 = if
+		RO_days == no -> false;
+		(Gregorian_day_today - Gregorian_day_log) >= RO_days -> 
+			true;
+		true -> false
+	end,
+
+	case lists:any(fun(E) -> E end, [Rotate1, Rotate2, Rotate3]) of
+		true -> 
+			{IoDevice2, Filename2, Gregorian_day2} = rotate_log(IoDevice, Logdir, Host),
+			{IoDevice2, Filename2, Gregorian_day2, 0};
+		false -> 
+			{IoDevice, Filename, Gregorian_day_log, PacketC+1}
+	end.
+
+filter(FilterO, E) ->
+	{{orientation, OrientationO},{stanza, StanzaO},{direction, DirectionO}} = FilterO,
+	{Orientation, From, To, Packet} = E,
+	
+	{xmlelement, Stanza_str, _Attrs, _Els} = Packet,
+	Stanza = list_to_atom(Stanza_str),
+
+	Hosts_all = ejabberd_config:get_global_option(hosts),
+	{Host_local, Host_remote} = case Orientation of
+		send -> {From#jid.lserver, To#jid.lserver};
+		recv -> {To#jid.lserver, From#jid.lserver}
+	end,
+	Direction = case Host_remote of
+		Host_local -> internal;
+		_ -> 
+			case lists:member(Host_remote, Hosts_all) of
+				true -> vhosts;
+				false -> external
+			end
+	end,
+
+	{lists:all(fun(O) -> O end, 
+		[lists:member(Orientation, OrientationO),
+		lists:member(Stanza, StanzaO),
+		lists:member(Direction, DirectionO)]),
+	{Orientation, Stanza, Direction}}. 
+
+loop(Host, IoDevice, Filename, Logdir, CheckRKP, RotateO, PacketC, Gregorian_day, FilterO) ->
+    receive
+        {addlog, E} ->
+			{IoDevice3, Filename3, Gregorian_day3, PacketC3} = case filter(FilterO, E) of
+				{true, OSD} ->
+					Div = PacketC/CheckRKP,
+					{IoDevice2, Filename2, Gregorian_day2, PacketC2} = case Div==round(Div) of
+						true ->
+							manage_rotate(Host, IoDevice, Filename, Logdir, RotateO, PacketC, Gregorian_day);
+						false ->
+							{IoDevice, Filename, Gregorian_day, PacketC+1}
+					end,
+					add_log(IoDevice2, E, OSD),
+					{IoDevice2, Filename2, Gregorian_day2, PacketC2};
+				_ ->
+					{IoDevice, Filename, Gregorian_day, PacketC}
+				end,
+            loop(Host, IoDevice3, Filename3, Logdir, CheckRKP, RotateO, PacketC3, Gregorian_day3, FilterO);
+        stop ->
+			close_file(IoDevice),
+            ok;
+        _ ->
+            loop(Host, IoDevice, Filename, Logdir, CheckRKP, RotateO, PacketC, Gregorian_day, FilterO)
+    end.
+
+send_packet(FromJID, ToJID, P) ->
+	Host = FromJID#jid.lserver,
+    Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+	Proc ! {addlog, {send, FromJID, ToJID, P}}.
+
+receive_packet(_JID, From, To, P) ->
+	Host = To#jid.lserver,
+    Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+	Proc ! {addlog, {recv, From, To, P}}.
+
+add_log(IoDevice, {Orientation, From, To, Packet}, _OSD) ->
+	%{Orientation, Stanza, Direction} = OSD, 
+	LocalJID = case Orientation of
+		send -> From;
+		recv -> To
+	end,
+	TimestampISO = jlib:timestamp_to_iso(calendar:now_to_universal_time(now())),
+	io:fwrite(IoDevice, "<packet or=\"~p\" ljid=\"~s\" ts=\"~s\">~s</packet>~n", 
+		[Orientation, jlib:jid_to_string(LocalJID), TimestampISO, xml:element_to_string(Packet)]).
+
+%% -------------------
+%% File
+%% -------------------
+
+open_file(Logdir, Host) ->
+	TimeStamp = jlib:timestamp_to_iso(calendar:now_to_universal_time(now())),
+	Year = string:substr(TimeStamp, 1, 4),
+	Month = string:substr(TimeStamp, 5, 2),
+	Day = string:substr(TimeStamp, 7, 2),
+	Hour = string:substr(TimeStamp, 10, 2),
+	Min = string:substr(TimeStamp, 13, 2),
+	Sec = string:substr(TimeStamp, 16, 2),
+	S = "-",
+	Logname = lists:flatten([Host,S,Year,S,Month,S,Day,S,Hour,S,Min,S,Sec,".xml"]),
+    Filename = filename:join([Logdir, Logname]),
+
+	Gregorian_day = get_gregorian_day(),
+
+	% Open file, create if it does not exist, create parent dirs if needed
+	case file:read_file_info(Filename) of
+		{ok, _} ->
+			{ok, IoDevice} = file:open(Filename, [append]);
+		{error, enoent} ->
+			make_dir_rec(Logdir),
+			{ok, IoDevice} = file:open(Filename, [append]),
+			io:fwrite(IoDevice, "~s~n", ["<?xml version=\"1.0\"?>"]),
+			io:fwrite(IoDevice, "~s~n", ["<?xml-stylesheet href=\"xmpp.xsl\" type=\"text/xsl\"?>"]),
+			io:fwrite(IoDevice, "~s~n", ["<log>"])
+	end,
+	{IoDevice, Filename, Gregorian_day}.
+
+close_file(IoDevice) ->
+	io:fwrite(IoDevice, "~s~n", ["</log>"]),
+	file:close(IoDevice).
+
+rotate_log(IoDevice, Logdir, Host) ->
+	close_file(IoDevice),
+	open_file(Logdir, Host).
+
+make_dir_rec(Dir) ->
+	case file:read_file_info(Dir) of
+		{ok, _} ->
+			ok;
+		{error, enoent} ->
+			DirS = filename:split(Dir),
+			DirR = lists:sublist(DirS, length(DirS)-1),
+			make_dir_rec(filename:join(DirR)),
+			file:make_dir(Dir)
+	end.
+
+%% -------------------
+%% Utils
+%% -------------------
+
+get_gregorian_day() -> calendar:date_to_gregorian_days(date()).
