7111db58d17938db9b6e74cc0947bf0288a77a37
badlop <badlop@d5c1f840-3623-0410-9cd6-eede35f65a94>
2007-08-04 22:38:06 +0000

    * ChangeLog: New file to track changes.
    	* src/mod_cron.erl: Fixed indentation.

--- mod_cron/trunk/src/mod_cron.erl
+++ mod_cron/trunk/src/mod_cron.erl
@@ -7,16 +7,15 @@
 %%%----------------------------------------------------------------------
 
 -module(mod_cron).
--author('').
--vsn('$Revision$').
+-author('badlop@ono.com').
+-vsn('$Revision$ ').
 
 -behaviour(gen_mod).
 
--export([
-	ctl_process/3,
-	run_task/3,
-	start/2, 
-	stop/1]).
+-export([ctl_process/3,
+	 run_task/3,
+	 start/2, 
+	 stop/1]).
 
 -include("ejabberd_ctl.hrl").
 -include("ejabberd.hrl").
@@ -32,83 +31,83 @@ start(Host, Opts) ->
     ejabberd_ctl:register_commands(Host, command_list(), ?MODULE, ctl_process),
     Tasks = gen_mod:get_opt(tasks, Opts, []),
     catch ets:new(cron_tasks, [ordered_set, named_table, public, {keypos, 2}]),
-	[add_task(Host, Task) || Task <- Tasks].
+    [add_task(Host, Task) || Task <- Tasks].
 
 stop(Host) ->
     ejabberd_ctl:unregister_commands(Host, command_list(), ?MODULE, ctl_process),
-	% Delete tasks of this host
-	[delete_task(Task) || Task <- get_tasks(Host)].
+    %% Delete tasks of this host
+    [delete_task(Task) || Task <- get_tasks(Host)].
 
 
 %% ---------------------
 %% Task management 
 %% ---------------------
 
-% Method to add new task
+%% Method to add new task
 add_task(Host, Task) ->
-	{Time_num, Time_unit, Mod, Fun, Args} = Task,
+    {Time_num, Time_unit, Mod, Fun, Args} = Task,
 
-	% Convert to miliseconds
-	Time = case Time_unit of
-		seconds -> timer:seconds(Time_num);
-		minutes -> timer:minutes(Time_num);
-		hours -> timer:hours(Time_num);
-		days -> timer:hours(Time_num)*24
-	end,
+    %% Convert to miliseconds
+    Time = case Time_unit of
+	       seconds -> timer:seconds(Time_num);
+	       minutes -> timer:minutes(Time_num);
+	       hours -> timer:hours(Time_num);
+	       days -> timer:hours(Time_num)*24
+	   end,
 
-	% Start timer
-	{ok, TimerRef} = timer:apply_interval(Time, ?MODULE, run_task, [Mod, Fun, Args]),
+    %% Start timer
+    {ok, TimerRef} = timer:apply_interval(Time, ?MODULE, run_task, [Mod, Fun, Args]),
 
-	% Get new task identifier
-	TaskId = get_new_taskid(),
+    %% Get new task identifier
+    TaskId = get_new_taskid(),
 
-	% Store TRef
-	Taskr = #task{
-		taskid = TaskId,
-		timerref = TimerRef,
-		host = Host,
-		task = Task
-	},
+    %% Store TRef
+    Taskr = #task{
+      taskid = TaskId,
+      timerref = TimerRef,
+      host = Host,
+      task = Task
+     },
 
-	ets:insert(cron_tasks, Taskr).
+    ets:insert(cron_tasks, Taskr).
 
 get_new_taskid() ->
-	case ets:last(cron_tasks) of
-		'$end_of_table' -> 0;
-		Id -> Id + 1
-	end.
+    case ets:last(cron_tasks) of
+	'$end_of_table' -> 0;
+	Id -> Id + 1
+    end.
 
-% Method to run existing task
+%% Method to run existing task
 run_task(Mod, Fun, Args) ->
-	case catch apply(Mod, Fun, Args) of
-		{'EXIT', Reason} ->
-			?ERROR_MSG("Error in scheduled task ~p:~p~p:~n~p", [Mod, Fun, Args, Reason]);
-		{error, Reason} ->
-			?ERROR_MSG("Error in scheduled task ~p:~p~p:~n~p", [Mod, Fun, Args, Reason]);
-		ok ->
-			?INFO_MSG("Scheduled task ~p:~p~p finished ok", [Mod, Fun, Args]);
-		Res ->
-			?INFO_MSG("Scheduled task ~p:~p~p returned:~n~p", [Mod, Fun, Args, Res])
-	end.
+    case catch apply(Mod, Fun, Args) of
+	{'EXIT', Reason} ->
+	    ?ERROR_MSG("Error in scheduled task ~p:~p~p:~n~p", [Mod, Fun, Args, Reason]);
+	{error, Reason} ->
+	    ?ERROR_MSG("Error in scheduled task ~p:~p~p:~n~p", [Mod, Fun, Args, Reason]);
+	ok ->
+	    ?INFO_MSG("Scheduled task ~p:~p~p finished ok", [Mod, Fun, Args]);
+	Res ->
+	    ?INFO_MSG("Scheduled task ~p:~p~p returned:~n~p", [Mod, Fun, Args, Res])
+    end.
 
-% Method to delete task, given a taskid
+%% Method to delete task, given a taskid
 delete_taskid(TaskId) ->
-	[Task] = ets:lookup(cron_tasks, TaskId),
-	delete_task(Task).
+    [Task] = ets:lookup(cron_tasks, TaskId),
+    delete_task(Task).
 
-% Method to delete task, given the whole task
+%% Method to delete task, given the whole task
 delete_task(Task) ->
-	timer:cancel(Task#task.timerref),
-	ets:delete(cron_tasks, Task#task.taskid).
+    timer:cancel(Task#task.timerref),
+    ets:delete(cron_tasks, Task#task.taskid).
 
-% Method to know existing tasks on a given host
+%% Method to know existing tasks on a given host
 get_tasks(Host) ->
-	ets:select(cron_tasks, [{#task{host = Host, _ = '_'}, [], ['$_']}]).
+    ets:select(cron_tasks, [{#task{host = Host, _ = '_'}, [], ['$_']}]).
 
-% Method to know taskids of existing tasks on a given host
-%get_tasks_ids(Host) ->
-%	L = ets:match(cron_tasks, #task{host = Host, taskid = '$1', _ = '_'}),
-%	[Id || [Id] <- L].
+%% Method to know taskids of existing tasks on a given host
+%%get_tasks_ids(Host) ->
+%%	L = ets:match(cron_tasks, #task{host = Host, taskid = '$1', _ = '_'}),
+%%	[Id || [Id] <- L].
 
 
 %% ---------------------
@@ -116,21 +115,21 @@ get_tasks(Host) ->
 %% ---------------------
 
 command_list() ->
-	[
-		{"cron-list", "list scheduled tasks"},
-		{"cron-del taskid", "delete this task from the schedule"}
-	].
+    [
+     {"cron-list", "list scheduled tasks"},
+     {"cron-del taskid", "delete this task from the schedule"}
+    ].
 
 ctl_process(_Val, Host, ["cron-list"]) ->
-	Tasks = get_tasks(Host),
+    Tasks = get_tasks(Host),
     [io:format("~p ~p~n", [T#task.taskid, T#task.task]) || T <- Tasks],
     ?STATUS_SUCCESS;
 
 ctl_process(_Val, _Host, ["cron-del", TaskId_string]) ->
-	TaskId = list_to_integer(TaskId_string),
+    TaskId = list_to_integer(TaskId_string),
     Result = delete_taskid(TaskId),
-	io:format("~p~n", [Result]),
+    io:format("~p~n", [Result]),
     ?STATUS_SUCCESS;
 
 ctl_process(Val, _Host, _Args) ->
-	Val.
+    Val.
