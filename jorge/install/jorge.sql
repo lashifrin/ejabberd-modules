332b4b6f18dda032ae839df38c37401f4731616d
kolargol <kolargol@d5c1f840-3623-0410-9cd6-eede35f65a94>
2007-08-14 13:08:01 +0000

    - updated SQL schema, added CREATE VIEW - removed unused patches and converters as new mod_logdb now supporting all this... - updated readme & todo list

--- jorge/install/jorge.sql
+++ jorge/install/jorge.sql
@@ -1,4 +1,3 @@
--- SQL definitions for Jorge @ 2007 Zbyszek Zolkiewski
 --
 -- Table structure for table `jorge_logger`
 --
@@ -24,7 +23,7 @@ CREATE TABLE `jorge_logger_dict` (
   `lang` char(3) default NULL,
   PRIMARY KEY  (`id_event`),
   KEY `jorge_logger_dict_idx` (`id_event`,`lang`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `jorge_logger_level_dict`
@@ -36,8 +35,7 @@ CREATE TABLE `jorge_logger_level_dict` (
   `level` varchar(20) default NULL,
   `lang` char(3) default NULL,
   PRIMARY KEY  (`id_level`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `jorge_mylinks`
@@ -53,21 +51,64 @@ CREATE TABLE `jorge_mylinks` (
   `link` text,
   `description` text,
   PRIMARY KEY  (`id_link`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=179 DEFAULT CHARSET=latin1;
 
 --
--- Fill dictionary with predefined values
+-- Insert data into dictionary
 --
 
 LOCK TABLES `jorge_logger_dict` WRITE;
-INSERT INTO `jorge_logger_dict` VALUES (1,'Logged in','eng'),(2,'Logged out','eng'),(3,'Login failed','eng'),(4,'Deleted chat thread','eng'),(5,'Deleted whole archive','eng'),(6,'Turned off archivization','eng'),(7,'Turned on archivization','eng');
+/*!40000 ALTER TABLE `jorge_logger_dict` DISABLE KEYS */;
+INSERT INTO `jorge_logger_dict` VALUES (1,'Logged in','eng'),(2,'Logged out','eng'),(3,'Login failed','eng'),(4,'Deleted chat thread','eng'),(5,'Deleted whole archive','eng'),(6,'Turned off archivization','eng'),(7,'Turned on archivization','eng'),(8,'Chat exported','eng'),(9,'Deleted entire archive','eng');
+/*!40000 ALTER TABLE `jorge_logger_dict` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Fill level dictionary
---
 
 LOCK TABLES `jorge_logger_level_dict` WRITE;
+/*!40000 ALTER TABLE `jorge_logger_level_dict` DISABLE KEYS */;
 INSERT INTO `jorge_logger_level_dict` VALUES (1,'normal','eng'),(2,'warn','eng'),(3,'alert','eng');
+/*!40000 ALTER TABLE `jorge_logger_level_dict` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Lets create view for jorge_logger for out convinience
+--
+
+CREATE VIEW v_jorge_logger AS 
+
+SELECT b.username AS username, c.level AS level, d.event AS event, a.log_time AS log_time, a.extra AS extra 
+	
+FROM 
+	jorge_logger a, 
+	jorge_logger_dict d, 
+	jorge_logger_level_dict c, 
+	logdb_users_jabber_autocom_pl b 
+	
+WHERE 
+b.user_id = a.id_user and 
+c.id_level = a.id_log_level and 
+d.id_event = a.id_log_detail;
+
+--
+-- Here is little tip: message view for your convinience. You can use this JOINS as primary data source:
+
+
+-- CREATE VIEW `v_logdb_messages_YOUR_DATE_TABLE_YOUR_XMPP_SERVER` AS
+-- 
+-- SELECT a.username AS user_name, b.username AS peer_name, c.server, d.resource, f.direction, e.type, f.subject, f.body, f.timestamp 
+--
+-- FROM 
+--  `logdb_users_YOUR_XMPP_SERVER` a, 
+--  `logdb_users_YOUR_XMPP_SERVER` b,
+--  `logdb_servers_YOUR_XMPP_SERVER` c, 
+--  `logdb_resources_YOUR_XMPP_SERVER` d, 
+--  `logdb_types_YOUR_XMPP_SERVER` e,
+--  `logdb_messages_YOUR_DATE_TABLE_YOUR_XMPP_SERVER` f
+--
+-- WHERE 
+-- a.user_id=f.owner_id and 
+-- b.user_id=f.peer_name_id and
+-- c.server_id=f.peer_server_id and 
+-- d.resource_id=f.peer_resource_id and
+-- e.type_id=f.type_id;
 
