e88cd1974357d06338eeb49d4896a3949d66d84c
badlop <badlop@d5c1f840-3623-0410-9cd6-eede35f65a94>
2007-08-21 21:16:24 +0000

    * mod_xmlrpc/*: Initial commit.

---
+++ mod_xmlrpc/trunk/README.txt
@@ -0,0 +1,184 @@
+
+        mod_xmlrpc - XML-RPC server
+
+        http://ejabberd.jabber.ru/mod_xmlrpc
+        (C) 2005-2006 by Badlop 
+
+
+	DESCRIPTION
+	-----------
+
+mod_xmlrpc is a module for ejabberd, a XMPP/Jabber server written in Erlang.
+It starts a XML-RPC server and waits for external requests. Implemented calls
+include statistics and user administration. This allows external programs written
+in any language like websites or administrative tools to communicate with 
+ejabberd to get information or to make changes without the need to know ejabberd 
+internals.
+    
+One example usage is a corporate site in PHP that creates a Jabber user every time
+a new user is created on the website.
+
+Some benefits of interfacing with the Jabber server by XML-RPC instead of modifying
+directly the database are:
+ - external programs are more simple and easy to develop and debug
+ - can communicate with a server in a different machine, and even on Internet
+
+
+	REQUIREMENTS
+	------------
+
+    Xmerl 0.20
+    XML-RPC-Erlang 1.13 with the IP patch
+    optional: mod_stats2 for statistics
+
+ - Install XMERL
+
+wget http://kent.dl.sourceforge.net/sourceforge/sowap/xmerl-0.20.tar.gz              
+tar -xzvf xmerl-0.20.tar.gz
+mv xmerl-0.20 xmerl
+cd xmerl
+make
+cd ..
+
+ - Install XMLRPC-E
+
+wget http://ejabberd.jabber.ru/files/contributions/xmlrpc-1.13-ip.tgz
+tar -xzvf xmlrpc-1.13-ip.tgz
+cd xmlrpc-1.13/src
+make
+cd ../../
+
+
+	INSTALL
+	-------
+
+1. Copy mod_xmlrpc.erl to the ejabberd src directory.
+
+2. Compile ejabberd. The proccess will show:
+
+	...
+	Recompile: mod_xmlrpc
+	...
+
+3. Add an option like this to the ejabberd execution script:
+
+$ erl -pa '/home/jabber/xmlrpc-1.13/ebin' ...
+
+4. Configure ejabberd to start this module at startup: 
+edit ejabberd.cfg and add on 'modules' section:
+
+	{mod_xmlrpc,      []},
+
+5. Now start ejabberd. If everything went ok, no mention to mod_xmlrpc 
+will be shown on the error log.
+
+
+	CONFIGURE
+	---------
+
+Configuration is done in 'modules' section in ejabberd.cfg. Available options:
+    port: port where the XML-RPC server will listen. Default: 4560
+    ip: IP address to listen, in Erlang format. Set to 'all' to listen on all IP address. Default: all
+
+Example configurations:
+    {mod_xmlrpc,     [{port, 4560}]}
+
+    {mod_xmlrpc,     [{port, 4560}, {ip, all}]}
+
+    {mod_xmlrpc,     [{port, 4560}, {ip, {81, 202, 202, 79}}]}
+
+
+
+	USAGE
+	-----
+
+You can send calls to http://host:4560/ 
+
+Call:           Arguments:                                                  Returns:
+
+ -- debug
+echothis        String                                                      String
+multhis         struct[{a, Integer}, {b, Integer}]                          Integer
+
+ -- statistics
+tellme_title    String                                                      String
+tellme_value    String                                                      String
+tellme          String                     struct[{title, String}, {value. String}]
+
+ -- user administration
+create_account  struct[{user, String}, {host, String}, {password, String}]  Integer
+change_password struct[{user, String}, {host, String}, {newpass, String}]   Integer
+num_resources   struct[{user, String}, {host, String}]                      Integer
+resource_num    struct[{user, String}, {host, String}, {num, Integer}]      String
+set_nickname    struct[{user, String}, {host, String}, {nickname, String}]  Integer
+add_rosteritem  struct[{localuser, String}, {localserver, String}, 
+                 {user, String}, {server, String}, 
+				 {nick, String}, {group, String}, {subs, String}]           String
+
+
+	TEST
+	----
+
+ - You can easily try the XML-RPC server starting a new Erlang Virtual Machine 
+   and making calls to ejabberd's XML-RPC:
+
+1. Start Erlang with this option:
+$ erl -pa '/home/jabber/xmlrpc-1.13/ebin'
+
+2. Now on the Erlang console, write commands and check the results:
+
+1> xmlrpc:call({127, 0, 0, 1}, 4560, "/", {call, echothis, [800]}).
+{ok,{response,[800]}}
+
+2> xmlrpc:call({127, 0, 0, 1}, 4560, "/", {call, echothis, ["blot cloc 557.889 kg"]}).
+{ok,{response,["blot cloc 557.889 kg"]}}
+
+3> xmlrpc:call({127, 0, 0, 1}, 4560, "/", {call, multhis, [{struct,[{a, 83}, {b, 689}]}]}).
+{ok,{response,[57187]}}
+
+4> xmlrpc:call({127, 0, 0, 1}, 4560, "/", {call, create_account, 
+[{struct, [{user, "ggeo"}, {host, "example.com"}, {password, "gogo11"}]}]}).
+{ok,{response,[0]}}
+
+5> xmlrpc:call({127, 0, 0, 1}, 4560, "/", {call, create_account, 
+[{struct, [{user, "ggeo"}, {host, "example.com"}, {password, "gogo11"}]}]}).
+{ok,{response,[409]}}
+
+
+ - Some possible XML-RPC error messages:
+
+   + Client: connection refused: wrong IP, wrong port, the server is not started...
+
+2> xmlrpc:call({127, 0, 0, 1}, 44444, "/", {call, echothis, [800]}).
+{error,econnrefused}
+
+   + Client: bad value: a800 is a string, so it must be put into ""
+
+7> xmlrpc:call({127, 0, 0, 1}, 4560, "/", {call, echothis, [a800]}).
+{error,{bad_value,a800}}
+
+   + Server: unknown call: you sent a call that the server does not implement 
+
+3> xmlrpc:call({127, 0, 0, 1}, 4560, "/", {call, bububu, [800]}).
+{ok,{response,{fault,-1,"Unknown call: {call,bububu,[800]}"}}}
+
+
+	TEST 2: PYTHON
+	--------------
+
+This is an example XML-RPC client in Python that you can use to get started (thanks to Diddek):
+-------
+  import xmlrpclib
+
+  server_url = 'http://127.0.0.1:4560';
+  server = xmlrpclib.Server(server_url);
+
+  params = {}
+  params["user"] = "ggeo"
+  params["host"] = "localhost"
+  params["password"] = "gogo11"
+
+  result = server.create_account(params)
+  print result
+-------
+
---
+++ mod_xmlrpc/trunk/src/mod_xmlrpc.erl
@@ -0,0 +1,231 @@
+%%%----------------------------------------------------------------------
+%%% File    : mod_xmlrpc.erl
+%%% Author  : Badlop <badlop@ono.com>
+%%% Purpose : XML-RPC server
+%%% Created : 21 Aug 2007 by Badlop <badlop@ono.com>
+%%% Id      : $Id$
+%%%----------------------------------------------------------------------
+
+-module(mod_xmlrpc).
+-author('badlop@ono.com').
+-vsn('$Revision$ ').
+
+-behaviour(gen_mod).
+
+-export([start/2,
+	 handler/2,
+	 loop/1,
+	 stop/1]).
+
+-include("ejabberd.hrl").
+-define(PROCNAME, ejabberd_mod_xmlrpc).
+
+
+%% -----------------------------
+%% Module interface
+%% -----------------------------
+
+start(_Host, Opts) -> 
+    case whereis(?PROCNAME) of
+	undefined ->
+	    %% get options
+	    Port = gen_mod:get_opt(port, Opts, 4560),
+	    MaxSessions = 10,
+	    Timeout = 50,
+	    Handler = {mod_xmlrpc, handler},
+	    State = started,
+
+	    Ip = gen_mod:get_opt(ip, Opts, all),
+
+	    %% start the XML-RPC server
+	    {ok, Pid} = xmlrpc:start_link(Ip, Port, MaxSessions, Timeout, Handler, State),
+
+	    %% start the loop process
+	    register(?PROCNAME, spawn(?MODULE, loop, [Pid]));
+	_ ->
+	    ok
+    end.
+
+loop(Pid) ->
+    receive
+        stop ->
+	    xmlrpc:stop(Pid)
+    end.
+
+stop(_Host) ->
+    ?PROCNAME ! stop.
+
+
+%% -----------------------------
+%% Handlers
+%% -----------------------------
+
+%% Call:           Arguments:                                      Returns:
+
+%% .............................
+%%  Debug
+
+%% echothis        String                                          String
+handler(_State, {call, echothis, [A]}) ->
+    {false, {response, [A]}};
+
+%% multhis         struct[{a, Integer}, {b, Integer}]              Integer
+handler(_State, {call, multhis, [{struct, [{a, A}, {b, B}]}]}) ->
+    {false, {response, [A*B]}};
+
+%% .............................
+%%  Statistics
+
+%% tellme_title    String                                          String
+handler(_State, {call, tellme_title, [A]}) ->
+    {false, {response, [get_title(A)]}};
+
+%% tellme_value    String                                          String
+handler(_State, {call, tellme_value, [A]}) ->
+    N = node(),
+    {false, {response, [get_value(N, A)]}};
+
+%% tellme          String          struct[{title, String}, {value. String}]
+handler(_State, {call, tellme, [A]}) ->
+    N = node(),
+    T = {title, get_title(A)},
+    V = {value, get_value(N, A)},
+    R = {struct, [T, V]},
+    {false, {response, [R]}};
+
+%% .............................
+%%  User administration
+
+%% create_account  struct[{user, String}, {host, String}, {password, String}]      Integer
+handler(_State, {call, create_account, [{struct, AttrL}]}) ->
+    [U, H, P] = get_attrs([user, host, password], AttrL),
+    R = case jlib:nodeprep(U) of
+	    error ->
+		error;
+	    "" ->
+		error;
+	    _ ->
+		ejabberd_auth:try_register(U, H, P)
+	end,
+    case R of
+	{atomic, ok} ->
+	    {false, {response, [0]}};
+	{atomic, exists} ->
+	    {false, {response, [409]}};
+	_E ->
+	    {false, {response, [1]}}
+    end;
+
+%% change_password struct[{user, String}, {host, String}, {newpass, String}]       Integer
+handler(_State, {call, change_password, [{struct, AttrL}]}) ->
+    [U, H, P] = get_attrs([user, host, newpass], AttrL),
+    case ejabberd_auth:set_password(U, H, P) of
+	{atomic, ok} ->
+	    {false, {response, [0]}};
+	_ ->
+	    {false, {response, [1]}}
+    end;
+
+%% num_resources struct[{user, String}, {host, String}]                            Integer
+handler(_State, {call, num_resources, [{struct, AttrL}]}) ->
+    [U, H] = get_attrs([user, host], AttrL),
+    R = length(ejabberd_sm:get_user_resources(U, H)),
+    {false, {response, [R]}};
+
+%% resource_num struct[{user, String}, {host, String}, {num, Integer}]             String
+handler(_State, {call, resource_num, [{struct, AttrL}]}) ->
+    [U, H, N] = get_attrs([user, host, num], AttrL),
+    Resources = ejabberd_sm:get_user_resources(U, H),
+    case (0<N) and (N=<length(Resources)) of
+	true -> 
+	    R = lists:nth(N, Resources),
+	    {false, {response, [R]}};
+	false ->
+	    FaultString = lists:flatten(io_lib:format("Wrong resource number: ~p", [N])),
+	    {false, {response, {fault, -1, FaultString}}}
+    end;
+
+%%% set_nickname    struct[{user, String}, {host, String}, {nickname, String}]      Integer
+handler(_State, {call, set_nickname, [{struct, AttrL}]}) ->
+    [U, H, N] = get_attrs([user, host, nickname], AttrL),
+    R = mod_vcard:process_sm_iq(
+	  {jid, U, H, "", U, H, ""},
+	  {jid, U, H, "", U, H, ""},
+	  {iq, "", set, "", "en", 
+	   {xmlelement, "vCard", 
+	    [{"xmlns", "vcard-temp"}], [
+					{xmlelement, "NICKNAME", [], [{xmlcdata, N}]}
+				       ]
+	   }}),
+    case R of
+	{iq, [], result, [], _L, []} ->
+	    {false, {response, [0]}};
+	_ ->
+	    {false, {response, [1]}}
+    end;
+
+%% add_rosteritem  struct[{localuser, String}, {localserver, String}, 
+%%                  {user, String}, {server, String}, 
+%%                  {nick, String}, {group, String}, 
+%%                  {subs, String}]                                String
+handler(_State, {call, add_rosteritem, [{struct, AttrL}]}) ->
+    [Localuser, Localserver, User, Server, Nick, Group, Subs] = 
+	get_attrs([localuser, localserver, user, server, nick, group, subs], AttrL),
+    Node = node(),
+    R = case add_rosteritem(Localuser, Localserver, User, Server, Nick, Group, list_to_atom(Subs), []) of
+	    {atomic, ok} ->
+		0;
+	    {error, Reason} ->
+		io:format("Can't add roster item to user ~p@~p on node ~p: ~p~n",
+			  [Localuser, Localserver, Node, Reason]);
+	    {badrpc, Reason} ->
+		io:format("Can't add roster item to user ~p@~p on node ~p: ~p~n",
+			  [Localuser, Localserver, Node, Reason])
+	end,
+    {false, {response, [R]}};
+
+%% If no other guard matches
+handler(_State, Payload) ->
+    FaultString = lists:flatten(io_lib:format("Unknown call: ~p", [Payload])),
+    ?INFO_MSG("Unknown call: ~p", [Payload]),
+    {false, {response, {fault, -1, FaultString}}}.
+
+
+%% -----------------------------
+%% Internal
+%% -----------------------------
+
+get_title(A) -> mod_statsdx:get_title(A).
+get_value(N, A) -> mod_statsdx:get(N, [A]).
+
+
+add_rosteritem(LU, LS, User, Server, Nick, Group, Subscription, Xattrs) ->
+    subscribe(LU, LS, User, Server, Nick, Group, Subscription, Xattrs).
+
+subscribe(LU, LS, User, Server, Nick, Group, Subscription, Xattrs) ->
+    mnesia:transaction(
+      fun() ->
+	      mnesia:write({roster,
+			    {LU,LS,{User,Server,[]}}, % uj
+			    {LU,LS},                  % user
+			    {User,Server,[]},      % jid
+			    Nick,                  % name: "Mom", []
+			    Subscription,  % subscription: none, to=you see him, from=he sees you, both
+			    none,          % ask: out=send request, in=somebody requests you, none
+			    [Group],       % groups: ["Family"]
+			    Xattrs,        % xattrs: [{"category","conference"}]
+			    []             % xs: []
+			   })
+      end).
+
+get_attrs(Attribute_names, L) ->
+    [get_attr(A, L) || A <- Attribute_names].
+get_attr(A, L) ->
+    case lists:keysearch(A, 1, L) of 
+	{value, {A, Value}} -> Value;
+	false -> 
+	    %% Report the error and then force a crash
+	    ?ERROR_MSG("Attribute '~p' not found on the list of attributes provided on the call:~n ~p", [A, L]),
+	    attribute_not_found = A
+    end.
+
