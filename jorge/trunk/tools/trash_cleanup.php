65fb2d205f66eb0dad6852f21be6613bf2b38a71
kolargol <kolargol@d5c1f840-3623-0410-9cd6-eede35f65a94>
2008-11-14 09:41:23 +0000

    Updated trash cleanup script to support index of trashed items. Please update your scripts.

--- jorge/trunk/tools/trash_cleanup.php
+++ jorge/trunk/tools/trash_cleanup.php
@@ -2,7 +2,7 @@
 /*
 Jorge - frontend for mod_logdb - ejabberd server-side message archive module.
 
-Copyright (C) 2007 Zbigniew Zolkiewski
+Copyright (C) 2008 Zbigniew Zolkiewski
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -30,7 +30,7 @@ mysql_select_db ("_YOUR_DATABASE_NAME_") or die ("DB select failed\n");
 
 $xmpp_host="_YOUR_XMPP_HOST"; # Replace dot with underscore f.e.: jabber.org -> jabber_org
 
-$query="select owner_id, peer_name_id,peer_server_id,date as tslice from pending_del where timeframe < date_format((date_sub(curdate(),interval 1 month)), '%Y-%c-%e')";
+$query="SELECT owner_id, peer_name_id, peer_server_id, idx, date AS tslice FROM pending_del WHERE timeframe < date_format((date_sub(curdate(),interval 1 month)), '%Y-%c-%e')";
 $result=mysql_query($query);
 
 if (mysql_num_rows($result)>0) {
@@ -40,12 +40,14 @@ if (mysql_num_rows($result)>0) {
 	while($row=mysql_fetch_array($result)) {
 
 		$i++;
-		$ch_del="delete from `logdb_messages_$row[tslice]"."_$xmpp_host` where owner_id='$row[owner_id]' and peer_name_id='$row[peer_name_id]' and peer_server_id='$row[peer_server_id]' and ext = '1'";
-		$li_del="delete from jorge_mylinks where owner_id='$row[owner_id]' and ext='1' and peer_name_id = '$row[peer_name_id]' and peer_server_id='$row[peer_server_id]' and datat = '$row[tslice]'";
-		$pe_del="delete from pending_del where owner_id='$row[owner_id]' and peer_name_id = '$row[peer_name_id]' and peer_server_id='$row[peer_server_id]' and date='$row[tslice]'";
+		$ch_del="delete from `logdb_messages_$row[tslice]"."_$xmpp_host` where owner_id='$row[owner_id]' and peer_name_id='$row[peer_name_id]' and peer_server_id='$row[peer_server_id]' and ext = '$row[idx]'";
+		$li_del="delete from jorge_mylinks where owner_id='$row[owner_id]' and ext='$row[idx]' and peer_name_id = '$row[peer_name_id]' and peer_server_id='$row[peer_server_id]' and datat = '$row[tslice]'";
+		$fa_del="delete from jorge_favorites where owner_id='$row[owner_id]' and ext='$row[idx]' and peer_name_id = '$row[peer_name_id]' and peer_server_id='$row[peer_server_id]'";
+		$pe_del="delete from pending_del where owner_id='$row[owner_id]' and peer_name_id = '$row[peer_name_id]' and peer_server_id='$row[peer_server_id]' and date='$row[tslice]' and idx = '$row[idx]'";
 		mysql_query("$ch_del") or die("Error #1\n");
 		mysql_query("$li_del") or die("Error #2\n");
-		mysql_query("$pe_del") or die("Error #3\n");
+		mysql_query("$fa_del") or die("Error #3\n");
+		mysql_query("$pe_del") or die("Error #4\n");
 
 	}
 
