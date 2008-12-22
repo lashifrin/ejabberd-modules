f385135457ed9742502be72c8ba60c976b50b22f
badlop <badlop@d5c1f840-3623-0410-9cd6-eede35f65a94>
2008-12-22 12:18:36 +0000

    Update files to current ejabberd trunk. Include pubsub.hrl

--- ejabberd-dev/trunk/include/ejabberd.hrl
+++ ejabberd-dev/trunk/include/ejabberd.hrl
@@ -1,17 +1,48 @@
 %%%----------------------------------------------------------------------
-%%% File    : ejabberd.hrl
-%%% Author  : Alexey Shchepin <alexey@sevcom.net>
-%%% Purpose : 
-%%% Created : 17 Nov 2002 by Alexey Shchepin <alexey@sevcom.net>
-%%% Id      : $Id: ejabberd.hrl 689 2006-12-04 16:07:44Z mremond $
+%%%
+%%% ejabberd, Copyright (C) 2002-2008   ProcessOne
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%                         
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%
 %%%----------------------------------------------------------------------
-%-define(ejabberd_debug, true).
-%-define(DBGFSM, true).
 
--define(VERSION, "1.1.2").
+%% This macro returns a string of the ejabberd version running, e.g. "2.3.4"
+%% If the ejabberd application description isn't loaded, returns atom: undefined
+-define(VERSION, element(2, application:get_key(ejabberd,vsn))).
+
+-define(MYHOSTS, ejabberd_config:get_global_option(hosts)).
+-define(MYNAME, hd(ejabberd_config:get_global_option(hosts))).
+-define(MYLANG, ejabberd_config:get_global_option(language)).
+
+-define(MSGS_DIR, "msgs").
+-define(CONFIG_PATH, "ejabberd.cfg").
+-define(LOG_PATH, "ejabberd.log").
+
+-define(EJABBERD_URI, "http://www.process-one.net/en/ejabberd/").
+
+-define(S2STIMEOUT, 600000).
+
+%%-define(DBGFSM, true).
 
 %% ---------------------------------
 %% Logging mechanism
+
+%% Print in standard output
+-define(PRINT(Format, Args),
+    io:format(Format, Args)).
 
 -define(DEBUG(Format, Args),
     ejabberd_logger:debug_msg(?MODULE,?LINE,Format, Args)).
@@ -28,15 +59,3 @@
 -define(CRITICAL_MSG(Format, Args),
     ejabberd_logger:critical_msg(?MODULE,?LINE,Format, Args)).
 
--define(MYHOSTS, ejabberd_config:get_global_option(hosts)).
--define(MYNAME, hd(ejabberd_config:get_global_option(hosts))).
--define(S2STIMEOUT, 600000).
--define(MYLANG, ejabberd_config:get_global_option(language)).
-
--define(MSGS_DIR, "msgs").
--define(CONFIG_PATH, "ejabberd.cfg").
--define(LOG_PATH, "ejabberd.log").
-
--define(PRIVACY_SUPPORT, true).
-
--define(EJABBERD_URI, "http://ejabberd.jabber.ru").
