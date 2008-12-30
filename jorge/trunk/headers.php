8f661de560a00c84584acf219ff4b2b63c494287
kolargol <kolargol@d5c1f840-3623-0410-9cd6-eede35f65a94>
2008-12-30 12:36:43 +0000

    Updated Slimster URL

--- jorge/trunk/headers.php
+++ jorge/trunk/headers.php
@@ -289,7 +289,7 @@ $html->headers('
 
 	function smackzk()  {
 
-		window.open(\'https://gps.autocom.pl/ZKJabDEVJorgate/\',\'\',
+		window.open(\'https://jorge.jabster.pl/slimster/\',\'\',
 			\'location=no,toolbar=no,menubar=no,scrollbars=no,resizable, height=375,width=715\');
 
 		}
