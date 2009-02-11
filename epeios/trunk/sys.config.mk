18d0d870663565ef60f038b372af8abbb83b30a6
badlop <badlop@d5c1f840-3623-0410-9cd6-eede35f65a94>
2009-02-11 15:25:20 +0000

    Include example configuration for mod_muc. It seems lib_path doesn't need to be full path. Fix indentation.

--- epeios/trunk/sys.config.mk
+++ epeios/trunk/sys.config.mk
@@ -1,21 +1,53 @@
 [
-%% Please, use an absolute lib path matching your actual configuration
-%% Epeios container configuration:
-{epeios, [{epeios_name, "pubsub.localhost"},
-          {epeios_server_host, "localhost"},
+ %% Epeios container configuration:
+ {epeios,[{epeios_name, "muc.localhost"},
+	  {epeios_server_host, "localhost"},
           {epeios_server_port, 8888},
           {epeios_secret, "secret"},
-          {epeios_module, "mod_pubsub"},
-          {epeios_host_config, []},
           {epeios_db_path, "database"},
-          {epeios_lib_path, "/Users/mremond/epeios-1.0.0/lib/darwin-x86"}]},
+          %% Depending of your machine, change this path to the
+          %% directory of expat_erl.so ...
+          {epeios_lib_path, "lib/linux-x86"},
 
-%% Logging configuration:
-{kernel, [{error_logger, {file, "logs/epeios_app.log"}},
-          {start_ddll, true},
+          %% Example configuration for mod_muc
+          {epeios_module, "mod_muc"},
+          {epeios_host_config,
+	   [
+            %% ACL does not work in Epeios
+            %% Access works in Epeios, but is rather useless without ACL
+	    {{access, muc_admin, global}, [{deny, all}]},
+	    {{access, muc, global}, [{allow, all}]}
+	   ]},
+          {epeios_module_config,
+	   [
+	    {access, muc},
+	    {access_create, muc},
+	    {access_persistent, muc},
+	    {access_admin, muc_admin}
+	   ]}
+
+	  %% Example configuration for mod_pubsub
+	  %%{epeios_module, "mod_pubsub"},
+	  %%{epeios_host_config, []},
+	  %%{epeios_module_config,
+	  %% [
+	  %%  {plugins, ["default"]}
+	  %% ]}
+
+	 ]},
+
+ %% Logging configuration:
+ {kernel,[{error_logger, {file, "logs/epeios_app.log"}},
+	  {start_ddll, true},
           {start_disk_log, false},
           {start_os, true},
           {start_pg2, true},
           {start_timer, true}]},
-{sasl, [{sasl_error_logger, {file, "logs/epeios_sasl_log"}}]}
+ {sasl, [{sasl_error_logger, {file, "logs/epeios_sasl.log"}}]}
 ].
+
+
+%%% Local Variables:
+%%% mode: erlang
+%%% End:
+%%% vim: set filetype=erlang tabstop=8:
